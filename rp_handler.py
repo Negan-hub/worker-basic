953bfc2a13b155a9bfd4c80df0906b09dc5a7922
Negan-hub <niel.j.cruzat@gmail.com>
2025-04-11 07:05:53 +0800

    Update rp_handler.py

--- rp_handler.py
+++ rp_handler.py
@@ -1,32 +1,20 @@
 import runpod
-import base64
-import io
-from PIL import Image
-from diffusers import StableDiffusionPipeline
-import torch
+import time  
 
 def handler(event):
     print(f"Worker Start")
-    input_data = event['input']
+    input = event['input']
     
-    prompt = input_data.get('prompt', "A beautiful landscape")
-    
+    prompt = input.get('prompt')  
+    seconds = input.get('seconds', 0)  
+
     print(f"Received prompt: {prompt}")
+    print(f"Sleeping for {seconds} seconds...")
     
-    # Load the model
-    pipe = StableDiffusionPipeline.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16)
-    pipe = pipe.to("cuda")
+    # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
+    time.sleep(seconds)  
     
-    # Generate the image
-    image = pipe(prompt).images[0]
-    
-    # Convert image to base64 for returning
-    buffered = io.BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode()
-    
-    # Return the base64 encoded image
-    return f"data:image/png;base64,{img_str}"
+    return prompt 
 
 if __name__ == '__main__':
-    runpod.serverless.start({'handler': handler})
+    runpod.serverless.start({'handler': handler })
